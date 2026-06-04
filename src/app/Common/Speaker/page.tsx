2fc7511c7d194c204aa6019c458791066d4b3d05
Rakesh <nightmode33@gmail.com>
2026-06-04 13:58:04 +0530

    Done

---
+++ src/app/Common/Speaker/page.tsx
@@ -0,0 +1,20 @@
+
+export default function page()
+{
+    return(
+        <>
+        <section className="bg-[#191919]">
+        <div
+          className={`container min-h-[232px] flex items-center justify-center `}
+        >
+         
+          <h1 className="text-[40px] font-bold text-white">
+           
+              Speaker
+           
+          </h1>
+        </div>
+      </section>
+        </>
+    )
+}
--- src/app/Component/Navbar/Navbar.tsx
+++ src/app/Component/Navbar/Navbar.tsx
@@ -1,11 +1,16 @@
+
+"use client"
 import React from 'react'
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import { ShoppingCart } from "lucide-react";
 import Image from 'next/image';
 export default function Navbar() {
+  const pathname = usePathname();
   return (
     <>
-     <nav className="navbar bg-[#191919]">
+      <nav className="navbar bg-[#191919]">
         <div className="container">
           <div className="navigation flex justify-between py-8 ">
             <div className="logo">
@@ -13,25 +18,42 @@ export default function Navbar() {
                 <Image src="/nav/logo.png" alt="Logo" width={150} height={100} />
               </Link>
             </div>
-          
-          <div className="nav-links ">
-            <ul className="flex gap-12">
-              <li className="nav-item  text-md font-bold hover:text-orange-500 transition-all duration-700"><Link href="/">Home</Link></li>
-              <li className="nav-item  text-md font-bold hover:text-orange-500 transition-all duration-700"><Link href="/Common/Headphone">Headphones</Link></li>
-              <li className="nav-item  text-md font-bold hover:text-orange-500 transition-all duration-700"><Link href="/contact">Speakers</Link></li>
-              <li className="nav-item  text-md font-bold hover:text-orange-500 transition-all duration-700"><Link href="/contact">Earphones</Link></li>
-            </ul>
-          </div>
-          <div className="nav-cart">
-            <ShoppingCart className='cursor-pointer hover:text-orange-500 transition-all duration-700' />
-          </div>
+
+            <div className="nav-links ">
+              <ul className="flex gap-12">
+                <li className={`text-md font-bold transition-all duration-700 ${pathname === "/"
+                    ? "text-orange-500"
+                    : "hover:text-orange-500"
+                  }`}>
+                  <Link href="/">Home</Link>
+                </li>
+
+                <li className={`text-md font-bold transition-all duration-700 ${pathname === "/Common/Headphone"
+                    ? "text-orange-500"
+                    : "hover:text-orange-500"
+                  }`}>
+                  <Link href="/Common/Headphone">Headphones</Link>
+                </li>
+
+                <li className={`text-md font-bold transition-all duration-700 ${pathname === "/Common/Speaker"
+                    ? "text-orange-500"
+                    : "hover:text-orange-500"
+                  }`}>
+                  <Link href="/Common/Speaker">Speakers</Link>
+                </li>
+              </ul>
+            </div>
+            <div className="nav-cart">
+              <ShoppingCart className='cursor-pointer hover:text-orange-500 transition-all duration-700' />
+            </div>
           </div>
           <hr />
 
         </div>
-        
-     </nav>
-    </>
-   
 
-    )}
+      </nav>
+    </>
+
+
+  )
+}
